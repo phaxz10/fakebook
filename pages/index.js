292453d7909cb29dbdacd3c922a4d0a9ac03245c
unknown <phaxz.bautista10@gmail.com>
2021-06-01 11:58:12 +0800

    test login page

--- pages/index.js
+++ pages/index.js
@@ -11,70 +11,70 @@ import { db } from '../firebase';
 // import Login from '../components/Login';
 
 export default function Home({ session, chatMessages }) {
-	const [chatWindow, setChatWIndow] = useState(false);
-	const toggleChatWindow = () => {
-		setChatWIndow(!chatWindow);
-	};
+  const [chatWindow, setChatWIndow] = useState(false);
+  const toggleChatWindow = () => {
+    setChatWIndow(!chatWindow);
+  };
 
-	if (!session) {
-		// return <Login />;
-		signIn();
+  if (!session) {
+    return <Login />;
+    // signIn();
 
-		return <></>;
-	}
-	return (
-		<div className='h-screen overflow-visible overflow-y-hidden bg-gray-100'>
-			<Head>
-				<title>Clone</title>
-				<meta name='description' content='This is a clone app' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			{/* header */}
-			<div className='sticky top-0 bg-white z-50'>
-				<Header toggleChatWindow={toggleChatWindow} />
-			</div>
+    return <></>;
+  }
+  return (
+    <div className='h-screen overflow-visible overflow-y-hidden bg-gray-100'>
+      <Head>
+        <title>Clone</title>
+        <meta name='description' content='This is a clone app' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      {/* header */}
+      <div className='sticky top-0 bg-white z-50'>
+        <Header toggleChatWindow={toggleChatWindow} />
+      </div>
 
-			<main className='flex overflow-y-scroll'>
-				{/* sidebar */}
-				<Sidebar />
-				{/* body */}
-				<Feed />
-				{/* right side */}
-				<Widget />
-				{/* chat */}
-				{chatWindow && (
-					<Chat
-						chatMessages={chatMessages}
-						session={session}
-						toggleChatWindow={toggleChatWindow}
-					/>
-				)}
-			</main>
-			<PencilAltIcon
-				onClick={toggleChatWindow}
-				className='h-14 rounded-full bg-gray-400 hover:bg-gray-600 text-white p-4 hidden md:flex absolute right-2 bottom-2'
-			/>
-		</div>
-	);
+      <main className='flex overflow-y-scroll'>
+        {/* sidebar */}
+        <Sidebar />
+        {/* body */}
+        <Feed />
+        {/* right side */}
+        <Widget />
+        {/* chat */}
+        {chatWindow && (
+          <Chat
+            chatMessages={chatMessages}
+            session={session}
+            toggleChatWindow={toggleChatWindow}
+          />
+        )}
+      </main>
+      <PencilAltIcon
+        onClick={toggleChatWindow}
+        className='h-14 rounded-full bg-gray-400 hover:bg-gray-600 text-white p-4 hidden md:flex absolute right-2 bottom-2'
+      />
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-	const session = await getSession(context);
-	const chatMessages = await db
-		.collection('chat')
-		.orderBy('timestamp', 'asc')
-		.get();
+  const session = await getSession(context);
+  const chatMessages = await db
+    .collection('chat')
+    .orderBy('timestamp', 'asc')
+    .get();
 
-	const chatData = chatMessages.docs.map((data) => ({
-		id: data.id,
-		...data.data(),
-		timestamp: null,
-	}));
+  const chatData = chatMessages.docs.map((data) => ({
+    id: data.id,
+    ...data.data(),
+    timestamp: null,
+  }));
 
-	return {
-		props: {
-			session,
-			chatMessages: chatData,
-		}, // will be passed to the page component as props
-	};
+  return {
+    props: {
+      session,
+      chatMessages: chatData,
+    }, // will be passed to the page component as props
+  };
 }
